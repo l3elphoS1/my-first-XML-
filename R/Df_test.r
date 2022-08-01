bba9fe5c55ca92891630b9b78300aa211fdbc68c
l3elphoS1 <110003636+l3elphoS1@users.noreply.github.com>
2022-08-01 21:17:11 +0700

    Create Df_test.r

---
+++ R/Df_test.r
@@ -0,0 +1,18 @@
+friends <- c("wan", "Ink", "Ana",
+             "Bee","Top")
+             
+ages <- c(26, 27, 32, 31, 28)
+
+locations <- c("New York","London",
+               "London", "Tokyo", 
+               "Manchester")
+
+movie_lover <- c(TRUE, TRUE, FALSE,
+                 TRUE, TRUE)
+
+df <- data.frame(friends,   
+           ages,
+           locations,
+           movie_lover)
+           
+View(df) 

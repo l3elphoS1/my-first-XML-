ffa89125dec10be948e541923fc72132a2c310eb
l3elphoS1 <110003636+l3elphoS1@users.noreply.github.com>
2022-08-01 21:19:39 +0700

    Create Grading_function.r

---
+++ R/Grading_function.r
@@ -0,0 +1,22 @@
+check_grade <- function(score){
+  if (score >= 80) {
+    print("Passed")
+  } else {
+    print("Failed")
+  }
+  
+  
+  # Multiple check
+  
+  check_grade <- function(score){
+  if (score >= 80) {
+    print("A")
+  } else if (score >= 70){
+    print("B")
+  } else if (score >= 60){
+      print("C")
+  } else if (score >= 50){
+     print ("D")
+  } else {print("Failed")
+    }
+}
